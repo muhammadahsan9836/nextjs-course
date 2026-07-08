8dfdd5e195f57065cf2f62d59ee91086622c5488
Muhammad Ahsan <malikahsan9836@gmail.com>
2026-07-08 14:57:34 +0500

    Remove TS type annotation from JS file to fix build

--- huntingcoder/app/api/postcontact/route.js
+++ huntingcoder/app/api/postcontact/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-export async function POST(req: Request) {
+export async function POST(req) {
   try {
     const data = await req.json();
     
